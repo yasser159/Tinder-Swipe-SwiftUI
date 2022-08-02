54d97436f3f0ece2bdf80b2125bcde920684201e
Yasser Hajlaoui <yasser159@gmail.com>
2022-08-02 11:52:41 -0700

    Initial Commit

---
+++ Tinder Swipe/Tinder_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_SwipeApp.swift
+//  Tinder Swipe
+//
+//  Created by Yasser Hajlaoui on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
